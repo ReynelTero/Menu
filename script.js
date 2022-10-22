80bca5b3626f890b07472b3130191de680ac9c1d
Reynel Tero <reynelgonzagatero@gmail.com>
2022-10-22 18:14:49 +0800

    updated

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 const menu = [
     {
+        id: 1,
         img: 'images/image-1.jpg',
-        filter: 'meal',
+        category: 'meal',
         name: 'All in One',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 50
     },
     {
+        id: 2,
         img: 'images/image-2.jpg',
-        filter: 'meal',
+        category: 'meal',
         name: 'Nuggets',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 5
     },
     {
+        id: 3,
         img: 'images/image-3.jpg',
-        filter: 'appetizer',
+        category: 'snacks',
         name: 'French Fries',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 7
     },
     {
+        id: 4,
         img: 'images/image-4.jpg',
-        filter: 'drink',
+        category: 'drink',
         name: 'Sprite',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 10
     },
     {
+        id: 5,
         img: 'images/image-5.jpg',
-        filter: 'drink',
+        category: 'drink',
         name: 'Coca-Cola',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 10
     },
     {
+        id: 6,
         img: 'images/image-6.jpg',
-        filter: 'drink',
+        category: 'drink',
         name: 'Mc Cafe Shake',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 25
     },
     {
+        id: 7,
         img: 'images/image-7.jpg',
-        filter: 'meal',
+        category: 'meal',
         name: '4 in 1',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 30
     },
     {
+        id: 8,
         img: 'images/image-8.jpg',
-        filter: 'appetizer',
+        category: 'snacks',
         name: 'Hamburger',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 8
     },
     {
+        id: 9,
         img: 'images/image-9.jpg',
-        filter: 'appetizer',
+        category: 'snacks',
         name: 'Regular Fries',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, commodi.',
         price: 4
@@ -65,10 +74,17 @@ const menu = [
 ]
 
 const sectionItems = document.querySelector('.menu-container');
+const buttonContainer = document.querySelector('.button-container');
 
 window.addEventListener('DOMContentLoaded', () => {
-    let viewMenu = menu.map(item => {
-        
+    viewMenuItems(menu);
+    viewMenuButtons();
+});
+
+//VIEW MENU FUNCTION!!!!!!!!!
+function viewMenuItems(items) {
+    let viewMenu = items.map(item => {
+    
         return `<section class="menu-container">
                     <div class="menu">
                         <img src=${item.img} class="photo" alt=${item.img}>
@@ -81,4 +97,46 @@ window.addEventListener('DOMContentLoaded', () => {
     })
     viewMenu = viewMenu.join('');
     sectionItems.innerHTML = viewMenu;
-})
+}
+//VIEW MENU FUNCTION!!!!!!
+
+function viewMenuButtons() {
+    const categories = menu.reduce(
+        (values, item) => {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+
+    let categoryBtns = categories.map(category => {
+        return `<button type="button" class="button" data-id=${category}>${category}</button>`;
+    })
+    categoryBtns = categoryBtns.join("");
+    buttonContainer.innerHTML = categoryBtns;
+
+    
+    const buttons = buttonContainer.querySelectorAll('.button');
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', (filteredId) => {
+            const category = filteredId.currentTarget.dataset.id;
+            const menuItemsCategory = menu.filter(items => {
+    
+                if (items.category === category) {
+                    return items;
+                }
+            });
+    
+            if (category === 'all') {
+                viewMenuItems(menu);
+            }
+    
+            else {
+                viewMenuItems(menuItemsCategory);
+            }
+        });
+    });
+}
